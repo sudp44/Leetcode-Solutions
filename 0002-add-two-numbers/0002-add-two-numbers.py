1ca8e669a294c6af10ad63c55682a0b72ea291f3
sudp44 <sp441998@gmail.com>
2026-07-18 16:58:15 +0530

    Time: 0 ms (100%), Space: 19.3 MB (78.49%) - LeetHub - Time: O(max(N, M)), Space: O(max(N, M))

---
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(1)
+        ans = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+
+            num = x+y+carry
+
+            carry = num//10
+            digit = num%10
+
+            ans.next = ListNode(digit)
+            ans = ans.next
+
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+
+        return dummy.next
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
