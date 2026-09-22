47bb61329d9f5d0911fd46369c15a8598ec4b739
sudp44 <sp441998@gmail.com>
2026-09-22 22:33:17 +0530

    Time: 71 ms (21.27%), Space: 21.9 MB (91.91%) - LeetHub - Time: O(H), Space: O(H)

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        parent_val = root.val
+        p_val = p.val
+        q_val = q.val
+
+        # If both p and q are greater than parent, LCA is in the right subtree
+        if p_val > parent_val and q_val > parent_val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        # If both p and q are smaller than parent, LCA is in the left subtree
+        elif p_val < parent_val and q_val < parent_val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        # Otherwise, we have found the split point (or one equals the parent)
+        else:
+            return root
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
